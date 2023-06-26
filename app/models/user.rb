55b8c5832ea6707e1478c8266ba6dce7c3a0e510
ikedoasako <124412564+ikedoasako@users.noreply.github.com>
2023-06-26 11:15:47 +0000

    新規会員登録機能実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   belongs_to :bushou
-  belongs_to :old_bushou, class_name: "Bushou", foreign_key: :old_bushou_id
+  belongs_to :old_bushou, class_name: "Bushou", foreign_key: :old_bushou_id, optional: true
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
