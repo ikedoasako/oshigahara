b1e528c3877e7d260b16ce89adf69603fbc490c0
ikedoasako <124412564+ikedoasako@users.noreply.github.com>
2023-07-01 08:30:03 +0000

    投稿詳細・編集機能実装

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,10 +1,17 @@
 class Admin::PostsController < ApplicationController
   def index
+    @posts = Post.all
+    @posts = Ppst.page(params[:page])
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:user_id, :bushou_id, :title, :body)
   end
 end
